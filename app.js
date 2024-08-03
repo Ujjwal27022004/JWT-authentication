a515b143fa6cc947994032c015899b653d592e0f
Ujjwal <ujjwal27022004@gmail.com>
2024-08-03 23:28:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+
+
+const app = express();
+
+
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser())
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://user:test1234@cluster0.lyyoiv9.mongodb.net/node-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes)
+
+
+//cookies
+// app.get("/set-cookies",(req,res)=>{
+//   //res.setHeader('Set-Cookie',"newUser=true")
+//   res.cookie('newUser',false)
+//   res.cookie('employee',true,{maxAge:1000})
+
+//   res.send("you got a coockie")
+// })
+
+
+// app.get("/read-cookies",(req,res)=>{
+
+//   const cookie = req.cookies
+//   console.log(cookie)
+
+//   res.json(cookie)
+// })
---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const {isEmail} = require('validator')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:[true,"Please enter an Email"],
+        unique:true,
+        lowercase:true,
+        validate:[isEmail,"Please enter an email"]
+    },
+    password:{
+        type:String,
+        required:[true,"Please enter an Password"],
+        minlength:[6,"Minimum password length is 6"]
+        
+    }
+})
+
+//fire a function after doc saved
+userSchema.post('save',function(doc,next){
+    
+    console.log("new user is created and saved",doc)
+    next();
+})
+//fire a function after doc saved
+userSchema.pre('save',async function(next){
+    // console.log("user about to get created",this)
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password,salt)
+    next();
+})
+
+const User = new mongoose.model('User',userSchema);
+module.exports=User
